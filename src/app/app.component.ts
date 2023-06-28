2df061bea2eee2433d4e0a8e528c09822d1b9bd4
Breidy <bjjaime@misena.edu.co>
2023-06-28 15:14:39 -0500

    proyecto angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+// el .ts fichero donde le vamos a dar la LOGICA 
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'store';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// vamos a tener un modelo especifico, es el modulo principal de la aplicacion 
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
